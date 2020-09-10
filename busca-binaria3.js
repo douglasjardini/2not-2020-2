014a7666cd37c1db6cacbf505d240ca213606666
douglasjardini <douglasjardini@gmail.com>
2020-09-10 23:31:37 +0000

    (10.09.2020)Busca Binaria - Implementação recursiva

--- busca-binaria3.js
+++ busca-binaria3.js
@@ -2,12 +2,18 @@
 //PRECISA estar ordenado pelo critério de busca
 let comp = 0
 
-//Implementação iterativa (baseada em loop)
-function buscaBinaria(lista,valorBusca, fnComp) {
-    let inicio = 0
-    let fim = lista.length -1
+//Implementação recursiva
+//uma função chama a si mesma (novamente) com pelo menos um de seus parametros alterados
+//Toda função recursiva precisa ter PELO MENOS uma condição de saída, isto é, uma possibilidade
+//de término que não envolve uma chamada a ela mesma
 
-    while(fim >= inicio) {
+//Os parametros início e fim são OPCIONAIS. Caso a função seja chamada sem especificá-los
+//eles assumirão os valores indicados
+function buscaBinaria(lista,valorBusca, fnComp, inicio = 0, fim = lista.length - 1) {
+    //let inicio = 0
+    //let fim = lista.length - 1
+    
+    if(fim >= inicio) {
         //Match.floor(): retira as casas decimais de um número
         let meio = Math.floor((fim + inicio) / 2)
 
@@ -16,18 +22,21 @@ function buscaBinaria(lista,valorBusca, fnComp) {
         //Verifica se o valor na posição média é o valor da busca
         if(res == 0) {
             comp++
-            return meio
+            return meio //Condição de saída (1º saída)
         }
         else if(res < 0) {
             comp += 2
-            fim = meio - 1
+            //fim = meio - 1
+            return buscaBinaria(lista, valorBusca, fnComp, inicio, meio - 1) //2º saída
         }
         else { //res > 0
             comp += 2
-            inicio = meio +1
+            //inicio = meio +1
+            return buscaBinaria(lista, valorBusca, fnComp, meio + 1, fim) //3º saída
         }
     }
-    return -1    //Valor não encontrado
+    //Condição de saída
+    return -1    //Valor não encontrado - 4º saída
 
 }
 
@@ -37,8 +46,19 @@ function comparaNome(obj,valorBusca) {
     //Um npumero negativo para indicar que o primeiro < que o segundo
     else if(valorBusca < obj.first_name) return -1
     //Um numero positivo para indicar que o primeiro > que o segundo
-    else 1 // valor busca> obj.first_name
+    else return 1 // valor busca> obj.first_name
 }
+
+let nums = [4, 16, 22, 29, 35, 44, 52, 58, 66, 71, 80, 88, 94]
+
+console.log(buscaBinaria(nums, 66, (elPos, busca) => {
+    if(busca === elPos) return 0
+    else if (busca < elPos) return -1
+    else return 1
+}))
+
+// Aqui termina a execução sem passar pelas linhas (buscas) abaixo
+//process.exit(0)  // 0 = saindo ok, sem erros
 
 let listaNomes = require('./dados/lista-nomes')
 
@@ -49,6 +69,7 @@ console.log('Comparações: ', comp)
 
 console.log("-------------------------------")
 comp = 0 
+
 console.time('ENEDINO')
 console.log(buscaBinaria(listaNomes, 'ENEDINO', (obj, bsusca) => {
     if(busca == obj.first_name) return 0
