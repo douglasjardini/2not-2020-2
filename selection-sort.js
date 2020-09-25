c64cc954b6002a5902b03719a064a6c43e655649
douglasjardini <douglasjardini@gmail.com>
2020-09-25 23:32:12 +0000

    (25.09.2020)Inicio do algoritmo MergeSort

--- selection-sort.js
+++ selection-sort.js
@@ -1,4 +1,11 @@
-function selectionSort(vetor) {
+/*
+    Função de comparação fnComp
+    - Recebe dois valores para comparação
+    - Retrno
+        - true se o PRIMEIRO valor for MAIOR que o segundo
+        - false se for o contrario    
+*/
+function selectionSort(vetor, fnComp) {
     let passadas = 0, comparacoes = 0, totalTrocas = 0
 
     //Função que encontra o meonor número em um seguimento de vetor (subvetor)
@@ -6,7 +13,8 @@ function selectionSort(vetor) {
     function encontrarMenor(vetor, inicio) {
         let posMenor = inicio
         for(let i = inicio + 1; i < vetor.length; i++) {
-            if(vetor[i] < vetor[posMenor]) posMenor = i
+            //if(vetor[i] < vetor[posMenor]) posMenor = i
+            if(! fnComp(vetor[i], vetor[posMenor])) posMenor = i
             comparacoes++
     }
     return posMenor
@@ -22,7 +30,8 @@ function selectionSort(vetor) {
         //Caso o valor encontrado seja menor que o valor atual,
         //procede-se à troca
         comparacoes++
-        if(vetor[posMenor] < vetor[i]) {
+        //if(vetor[posMenor] < vetor[i]) {
+            if(fnComp(vetor[i], vetor[posMenor])) { // Ordem dos artumentos invertido
             //Permuta de valores usando desestruturação
             [vetor[posMenor], vetor[i]] = [vetor[i] = vetor[posMenor]]
             totalTrocas++
@@ -31,10 +40,11 @@ function selectionSort(vetor) {
     console.log({passadas, comparacoes, totalTrocas})
 }
 
-const nums = [56, 78, 44, 23, 99, 14, 60, 37, 6, 82, 31, 65]
-
-console.time('nums')
-selectionSort('nums')
-console.timeEnd('nums')
-
-console.log(nums)
+let candidatos = require('./dados/candidatos-2018')
+console.time('Teste candidatos')
+selectionSort(candidatos, (x, y) => x.NN_CANDIDATO < y.NN_CANDIDATO)
+console.timeEnd('Teste candidatos')
+//Medindo a memória utilizada pelo programa
+let memoria = process.memoryUsage().heapUsed / 1024 / 1024
+console.log(candidatos)
+console.log('Memoria utilizada (MB):', memoria)
