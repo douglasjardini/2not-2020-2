11a5a1723c2b6e38eadd1de74be54869ed5dfcfa
douglasjardini <douglasjardini@gmail.com>
2020-10-23 23:32:14 +0000

    (23.10.2020)Finalização de pilhas

--- analisador.expr.js
+++ analisador.expr.js
@@ -4,11 +4,12 @@
 
 */
 
-const Stack = require('./lib/Stack')
+const Stack = require('./lib/Stack2')
 
 let analisador = new Stack()
 
 let expr = '6 + { 8 / [3 - (9 * 2)] + (4*3)}'
+//let expr = '6 + { 8 / [3 - (9 * 2)] + (4*3)} + (2 -'
 
 for(let i = 0; i < expr.length; i++) {
     switch(expr.charAt(i)) {
@@ -31,8 +32,45 @@ for(let i = 0; i < expr.length; i++) {
                 console.log(`Erro: Chave fechando na posção ${i} não tem abertura correspondente.`)
             }
             break
+        case ']':
+            info = analisador.pop()
+            // info não pode ser vazio e seu tipo de ser CO
+            if(info && info.tipo == 'CO') {
+                console.log(`Colchete aberto na posição ${info.pos} e fechando na posição ${i}.`)
+            }
+            else {
+                console.log(`Erro: colchete fechando na posição ${i} não tem abertura correspondente.`)
+            }
+            break
+        case ')':
+            info = analisador.pop()
+            // info não pode ser vazio e seu tipo de ser PA
+            if(info && info.tipo == 'PA') {
+                console.log(`Parenteses aberto na posição ${info.pos} e fechando na posição ${i}.`)
+            }
+            else {
+                console.log(`Erro: parenteses fechando na posição ${i} não tem abertura correspondente.`)
+            }            
     }
 
+}
+
+
+
+
+//Analisador residuo da pilha
+while(analisador.size() > 0) {  //Enquanto ainda houver elementos na pilha
+    info = analisador.pop()
+    switch(info.tipo) {
+        case 'PA':
+            console.log(`ERRO: Parentese aberto na posição ${info.pos} naõ tem o fechamento correspondente.`)
+            break
+        case 'CO':
+            console.log(`ERRO: Colchete aberto na posição ${info.pos} naõ tem o fechamento correspondente.`)
+            break
+        case 'CH':
+            console.log(`ERRO: Chave aberto na posição ${info.pos} naõ tem o fechamento correspondente.`)
+    }
 }
 
 console.log(analisador)
